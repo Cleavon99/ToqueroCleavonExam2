5f6e7f69a3fba3255f59dc38959b26518cdd2b89
Cleavon99 <2015083338@ust-ics.mygbiz.com>
2019-10-05 09:43:04 +0800

    Initial commit

---
+++ app/src/main/java/com/toquero/toquerocleavonexam2/MainActivity.java
@@ -0,0 +1,70 @@
+package com.toquero.toquerocleavonexam2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    CheckBox[] checkBox = new CheckBox[8];
+    EditText edit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edit = findViewById(R.id.editText);
+        checkBox[0] = findViewById(R.id.cb1);
+        checkBox[1] = findViewById(R.id.cb2);
+        checkBox[2] = findViewById(R.id.cb3);
+        checkBox[3] = findViewById(R.id.cb4);
+        checkBox[4] = findViewById(R.id.cb5);
+        checkBox[5] = findViewById(R.id.cb6);
+        checkBox[6] = findViewById(R.id.cb7);
+        checkBox[7] = findViewById(R.id.cb8);
+
+    }
+
+    public void save(View v){
+        String data = "";
+        FileOutputStream writer = null;
+        String checkedBoxes = "";
+
+        for(int i = 0; i < 8; i++){
+            if(checkBox[i].isChecked()){
+                checkedBoxes+=checkBox[i].getText().toString() +" ";
+            }
+        }
+        try {
+            String data1 = edit.getText().toString();
+            writer = openFileOutput("data5.txt", MODE_APPEND);
+            writer.write(checkedBoxes.getBytes());
+            writer.write(data1.getBytes());
+        }catch (IOException e){
+            Log.d("error", "IOException");
+        }finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                Log.d("error", "File not found 2");
+            }
+        }
+        Toast.makeText(this, "Data Saved...", Toast.LENGTH_LONG).show();
+    }
+
+    public void next(View v) {
+        Intent intent = new Intent(this, MainActivityTwo.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/toquero/toquerocleavonexam2/MainActivityTwo.java
@@ -0,0 +1,60 @@
+package com.toquero.toquerocleavonexam2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivityTwo extends AppCompatActivity {
+
+    CheckBox[] checkBox = new CheckBox[8];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maintwo);
+
+        FileInputStream reader = null;
+        String msg="";
+        try{
+            int token;
+            StringBuffer buffer = new StringBuffer();
+            reader = openFileInput("data5.txt");
+            while((token=reader.read())!=-1){
+                buffer.append((char)token);
+                msg=msg+(char)token;
+            }
+            reader.close();
+            String list[]=msg.split(" ");
+            ((TextView)(findViewById(R.id.tv1))).setText(list[0]);
+            ((TextView)(findViewById(R.id.tv2))).setText(list[1]);
+            ((TextView)(findViewById(R.id.tv3))).setText(list[2]);
+            ((TextView)(findViewById(R.id.tv4))).setText(list[3]);
+            ((TextView)(findViewById(R.id.tv5))).setText(list[4]);
+            ((TextView)(findViewById(R.id.tv6))).setText(list[5]);
+            ((TextView)(findViewById(R.id.tv7))).setText(list[6]);
+            ((TextView)(findViewById(R.id.tv8))).setText(list[7]);
+            ((TextView) (findViewById(R.id.tvcomment))).setText(list[9]);
+
+        }catch (FileNotFoundException e){
+            Log.d("error", "File not found error msg");
+        }catch (IOException e){
+            Log.d("error", "file not found");
+        }
+
+
+}
+    public void previous(View v){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+}
